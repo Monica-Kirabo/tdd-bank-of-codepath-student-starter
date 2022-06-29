eac5aaac96e275729993210f132b3e5c0d5a2f16
monica kirabo <kirabom@duq.edu>
2022-06-29 10:31:33 -0700

    commit here

--- bank-of-codepath-ui/src/components/App/App.jsx
+++ bank-of-codepath-ui/src/components/App/App.jsx
@@ -1,19 +1,25 @@
-
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import * as React from "react"
-import Navbar from "../Navbar/Navbar"
-import Home from "../Home/Home"
-import "./App.css"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import * as React from "react";
+import Navbar from "../Navbar/Navbar";
+import Home from "../Home/Home";
+import TransactionDetail from "../TransactionDetail/TransactionDetail";
+import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      
-      
+        <Navbar />
+        <main>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/transactions/:transactionId"
+              element={<TransactionDetail />}
+            />
+          </Routes>
+        </main>
       </BrowserRouter>
-      <Navbar />
-      <Home />
     </div>
-  )
+  );
 }
